1ea6f618d8a53bbe08c757a7ce0b2b4cd2c5f283
Panczel24 <pcsenge8@gmail.com>
2026-09-25 09:50:06 +0200

    kimaradt feladatok

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,76 +1,30 @@
 import { Controller, Get, Query, Render } from '@nestjs/common';
 import { AppService } from './app.service.js';
 import { Expense } from './Expense.js';
+import { stat } from 'fs';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
 
-  expense: Expense[] = [
-    {
-      name: 'Élelmiszer',
-      amount: 12500,
-      category: 'food',
-    },
-    {
-      name: 'Villanyszámla',
-      amount: 18500,
-      category: 'utilities',
-    },
-    {
-      name: 'Netflix',
-      amount: 3990,
-      category: 'entertainment',
-    },
-    {
-      name: 'Buszjegy',
-      amount: 2500,
-      category: 'misc',
-    },
-    {
-      name: 'Ebéd',
-      amount: 4200,
-      category: 'food',
-    },
-    {
-      name: 'Internet',
-      amount: 7500,
-      category: 'utilities',
-    },
-    {
-      name: 'Mozi',
-      amount: 4500,
-      category: 'entertainment',
-    },
-    {
-      name: 'Tisztítószerek',
-      amount: 6300,
-      category: 'misc',
-    },
-    {
-      name: 'Bevásárlás',
-      amount: 15600,
-      category: 'food',
-    },
-    {
-      name: 'Vízszámla',
-      amount: 5200,
-      category: 'utilities',
-    },
-  ];
-
 
 
   @Get()
   @Render('index')
-  getHello() {
-    const total = this.expense.reduce((sum, expense) => sum + expense.amount, 0)
-    return {
-      title: 'Összkiadas: ', total
-    }
-  }
+  getHome() {
+    // const total = this.expense.reduce(
+    //   (sum, expense) => sum + expense.amount,
+    //   0
+    // );
 
+    const total = this.appService.getTotal();
+
+    return {
+      title: 'Összkiadás',
+      total
+    };
+  }
 
 
   @Get('all')
@@ -78,58 +32,111 @@ export class AppController {
   getAll() {
     return {
       title: "Összes kiadás",
-      expense: this.expense
-    }
+      expense: this.appService.getExpenses()
+    };
   }
 
 
   @Get('top3')
   @Render("all")
   getTop3() {
-    const top3 = [...this.expense]
-      .sort((a, b) => b.amount - a.amount)
-      .slice(0, 3);
+   const top3 = this.appService.getTop3();
     return {
       title: "Top 3",
       expense: top3
-    }
-  }
-
-  @Get('search')
-  @Render("search")
-  getSearch(@Query('name') name?: string) {
-    const searchTerm = name?.toLocaleLowerCase() ?? '';
-    const results = this.expense.filter(expense =>
-      expense.name.toLocaleLowerCase().includes(searchTerm),
-    );
-
-
-    return {
-      title: 'Kiadás keresése',
-      expense: results,
-      searchTerm: searchTerm,
     };
   }
 
 
+  @Get('search')
+  @Render("search")
+  getSearch(@Query('name') name?: string) {
+    const searchTerm = name?? '';
+
+    const results = this.appService.searchByName(searchTerm)
+
+    return {
+      title: 'Kiadás keresése',
+      expense: results,
+      searchTerm: searchTerm
+    };
+  }
 
 
   @Get('expensive')
   @Render("expensive")
   getExpensive(@Query("amount") amount?: string) {
     const minAmount = Number(amount);
-    const results = this.expense.filter(
-      expense => expense.amount> minAmount
-    )
-  
+
+    const results =this.appService.getMoreExpensiveThan(minAmount)
+
     return {
       title: 'Drága',
       expense: results,
-      amount: amount?? '',
+      amount: amount ?? ''
     };
   }
 
 
+  @Get('stats')
+  @Render("stats")
+  getStats() {
+
+    const stats = this.appService.getStatistics();
+    return{
+      title:"statisztikák", ...stats
+    }
 
 
+
+
+
+    // const count = this.expense.length;
+
+    // const total = this.expense.reduce(
+    //   (sum, expense) => sum + expense.amount,
+    //   0
+    // );
+
+    // const avg = total / count;
+
+    // const categories = [
+    //   "food",
+    //   "utilities",
+    //   "entertainment",
+    //   "misc"
+    // ] as const;
+
+    // const categoryStats = categories.map(category => {
+    //   const categoryExpenses = this.expense.filter(expense => {
+    //     return expense.category === category;
+    //   });
+
+    //   const categoryCount = categoryExpenses.length;
+
+    //   const categoryTotal = categoryExpenses.reduce(
+    //     (sum, expense) => sum + expense.amount,
+    //     0
+    //   );
+
+    //   const categoryAvg = categoryCount > 0
+    //     ? categoryTotal / categoryCount
+    //     : 0;
+
+    //   return {
+    //     category,
+    //     count: categoryCount,
+    //     total: categoryTotal,
+    //     average: categoryAvg
+    //   };
+    // });
+
+    // return {
+    //   title: "Statisztikák",
+    //   count,
+    //   total,
+    //   avg,
+    //   categoryStats
+    // };
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,186 @@
 import { Injectable } from '@nestjs/common';
+import { Expense } from './Expense.js';
+import { filter, map } from 'rxjs';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  expense: Expense[] = [
+    {
+      name: 'Élelmiszer',
+      amount: 12500,
+      category: 'food',
+    },
+    {
+      name: 'Villanyszámla',
+      amount: 18500,
+      category: 'utilities',
+    },
+    {
+      name: 'Netflix',
+      amount: 3990,
+      category: 'entertainment',
+    },
+    {
+      name: 'Buszjegy',
+      amount: 2500,
+      category: 'misc',
+    },
+    {
+      name: 'Ebéd',
+      amount: 4200,
+      category: 'food',
+    },
+    {
+      name: 'Internet',
+      amount: 7500,
+      category: 'utilities',
+    },
+    {
+      name: 'Mozi',
+      amount: 4500,
+      category: 'entertainment',
+    },
+    {
+      name: 'Tisztítószerek',
+      amount: 6300,
+      category: 'misc',
+    },
+    {
+      name: 'Bevásárlás',
+      amount: 15600,
+      category: 'food',
+    },
+    {
+      name: 'Vízszámla',
+      amount: 5200,
+      category: 'utilities',
+    },
+    // {
+    //   name: 'BKK bérlet',
+    //   amount: 8950,
+    //   category: 'transport',
+    // },
+  ];
+
+
+
+
+
+
+  getExpenses(): Expense[] {
+    return this.expense;
+  }
+
+
+  getTotal(): number {
+    return this.expense.reduce((sum, expense) => sum + expense.amount, 0)
+  }
+
+
+  getTop3(): Expense[] {
+    return [...this.expense]
+      .sort((a, b) => b.amount - a.amount)
+      .slice(0, 3);
+  }
+
+
+  searchByName(name: string): Expense[] {
+    const searchTerm = name.toLowerCase();
+    return this.expense.filter(expense =>
+      expense.name.toLowerCase().includes(searchTerm)
+    )
+  }
+
+
+  getMoreExpensiveThan(amount: number): Expense[] {
+    return this.expense.filter(expense =>
+      expense.amount > amount
+    )
+  }
+
+
+  getStatistics() {
+
+
+
+    const count = this.expense.length;
+
+    const total = this.expense.reduce(
+      (sum, expense) => sum + expense.amount,
+      0
+    );
+
+    const avg = total / count;
+    const categoryMap = new Map<string, Expense[]>();
+    for (const expense of this.expense) {
+      if (!categoryMap.has(expense.category)) {
+        categoryMap.set(expense.category, [])
+
+      }
+      categoryMap.get(expense.category)!.push(expense);
+    }
+
+    const categoryStats = Array.from(categoryMap.entries()).map(
+      ([category, categoryExpenses]) => {
+        const categoryCount = categoryExpenses.length;
+        const categoryTotal = categoryExpenses.reduce((sum, expense) => sum + expense.amount, 0)
+        const categoryAvg = categoryCount > 0 ? categoryTotal / categoryCount : 0;
+
+        return {
+          category,
+          count: categoryCount,
+          total: categoryTotal,
+          average: categoryAvg
+        };
+
+      }
+    )
+
+    /*
+    const categories = [
+      "food",
+      "utilities",
+      "entertainment",
+      "misc"
+    ] as const;
+
+    const categoryStats = categories.map(category => {
+      const categoryExpenses = this.expense.filter(expense => {
+        return expense.category === category;
+      });
+
+      const categoryCount = categoryExpenses.length;
+
+      const categoryTotal = categoryExpenses.reduce(
+        (sum, expense) => sum + expense.amount,
+        0
+      );
+
+      const categoryAvg = categoryCount > 0
+        ? categoryTotal / categoryCount
+        : 0;
+
+      return {
+        category,
+        count: categoryCount,
+        total: categoryTotal,
+        average: categoryAvg
+      };
+    });
+
+    */
+
+    return {
+      count,
+      total,
+      avg,
+      categoryStats
+    };
+
+  }
+
 }
